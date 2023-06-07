ec5e376d29c0ac4f0f20606c4608cd6b2b7eed45
Gamareel <kmhasni15@gmail.com>
2023-06-07 17:30:26 +0100

    esp

--- temperature/temperature.ino
+++ temperature/temperature.ino
@@ -1,36 +1,24 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <WiFi.h>
 
-// Data wire is plugged into digital pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+const char* ssid = "Appah";
+const char* password = "12345678";
 
-// Setup a oneWire instance to communicate with any OneWire device
-OneWire oneWire(ONE_WIRE_BUS);	
-
-// Pass oneWire reference to DallasTemperature library
-DallasTemperature sensors(&oneWire);
-
-void setup(void)
-{
-  sensors.begin();	// Start up the library
+void setup() {
   Serial.begin(9600);
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop(void)
-{ 
-  // Send the command to get temperatures
-  sensors.requestTemperatures(); 
-
-  //print the temperature in Celsius
-  Serial.print("Temperature: ");
-  Serial.print(sensors.getTempCByIndex(0));
-  Serial.print((char)176);//shows degrees character
-  Serial.print("C  |  ");
-  
-  //print the temperature in Fahrenheit
-  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
-  Serial.print((char)176);//shows degrees character
-  Serial.println("F");
-  
-  delay(500);
+void loop() {
+  // Your code here
 }
