1d3138fd53c07bcbd097bb5e10446b6be824cf52
Gamareel <kmhasni15@gmail.com>
2023-06-07 17:56:54 +0100

    tt

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,10 +1,30 @@
+/* The true ESP32 chip ID is essentially its MAC address.
+This sketch provides an alternate chip ID that matches 
+the output of the ESP.getChipId() function on ESP8266 
+(i.e. a 32-bit integer matching the last 3 bytes of 
+the MAC address. This is less unique than the 
+MAC address chip ID, but is helpful when you need 
+an identifier that can be no more than a 32-bit integer 
+(like for switch...case).
+
+created 2020-06-07 by cweinhofer
+with help from Cicicok */
+	
+uint32_t chipId = 0;
+
 void setup() {
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("ESP32 Test");
+	Serial.begin(115200);
 }
 
 void loop() {
-  Serial.println("Hello, world!");
-  delay(1000);
+	for(int i=0; i<17; i=i+8) {
+	  chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+	}
+
+	Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
+	Serial.printf("This chip has %d cores\n", ESP.getChipCores());
+  Serial.print("Chip ID: "); Serial.println(chipId);
+  
+	delay(3000);
+
 }
